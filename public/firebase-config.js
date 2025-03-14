15a2ca49f62dba4f22ba54450889c29048ff244b
Sarmad Gardezi <mail@sarmadgardezi.com>
2025-03-14 12:27:25 +0500

    Update env

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -5,12 +5,13 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase
 
 // Firebase Configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA0HaZ8sCCLAgVaLouQoHygjzleMX60o-s",
-  authDomain: "test-pj-51c3d.firebaseapp.com",
-  projectId: "test-pj-51c3d",
-  storageBucket: "test-pj-51c3d.appspot.com",
-  messagingSenderId: "520194841599",
-  appId: "1:520194841599:web:962b91bee34847641165ca"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  databaseURL: "YOUR_DATABASE_URL",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
 // ✅ Initialize Firebase
